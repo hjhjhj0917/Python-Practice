2acda1ad889fcbd0a1308f3f2df4173fdb262ec7
hjhjhj0917 <abc22842199@gmail.com>
2025-10-04 13:30:46 +0900

    연습

---
+++ ex02.py
@@ -0,0 +1,7 @@
+a = int(input("값을 입력하세요 : "))
+b = int(input("값을 입력하세요 : "))
+
+print("a + b = %d" % (a + b))
+print("a - b = %d" % (a - b))
+print("a * b = %d" % (a * b))
+print("a / b = %d" % (a / b))
