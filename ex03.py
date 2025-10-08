a749af33900d06dd5c0203910549d3780bfb6bfb
hjhjhj0917 <abc22842199@gmail.com>
2025-10-08 23:33:26 +0900

    연습

---
+++ ex03.py
@@ -0,0 +1,12 @@
+import random
+
+myList = ['하나', '둘', '셋', '넷', '다섯', '여섯']
+
+while True:
+    response = input("주사위 던지기를 하시겠습니까? (yes or no) : ")
+
+    if response == "yes" :
+        coin = random.choice(myList)
+        print(coin)
+    else :
+        break
